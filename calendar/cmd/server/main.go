5eb85142dd621180d6bd2031177f92b55893ce92
Roman Savchyshyn <77-roma-77@url.net>
2022-08-01 15:19:28 +0300

    Support configuration

--- calendar/cmd/server/main.go
+++ calendar/cmd/server/main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
+	"github.com/Roma7-7-7/workshops/calendar/internal/config"
 	"github.com/Roma7-7-7/workshops/calendar/internal/repository/postgre"
 	"github.com/Roma7-7-7/workshops/calendar/internal/server/http"
 	"github.com/Roma7-7-7/workshops/calendar/internal/services/calendar"
 	"github.com/Roma7-7-7/workshops/calendar/internal/services/validator"
 	"github.com/gin-gonic/gin"
-	"os"
 )
 
 func main() {
-	var dsn string
-	if dsn = os.Getenv("DSN"); dsn == "" {
-		dsn = "user=gouser password=gopassword dbname=gotest sslmode=disable"
-	}
-	repo := postgre.NewRepository(dsn)
+	cfg := config.GetConfig()
+	repo := postgre.NewRepository(cfg.DSN())
 	service := calendar.NewService(repo)
 	server := http.NewServer(service, &validator.Service{})
 
--- calendar/internal/config/config.go
+++ calendar/internal/config/config.go
@@ -1,10 +1,79 @@
 package config
 
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"sync"
+)
+
 // Application holds application configuration values
 type Application struct {
 	DB *Database
 }
 
 type Database struct {
-	DSN string `env:"DSN"`
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	Name     string `yaml:"name"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	SSLMode  bool   `yaml:"sslmode"`
+}
+
+var instance *Application
+var once sync.Once
+
+func GetConfig() *Application {
+	once.Do(func() {
+		viper.AddConfigPath(".")
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+		viper.AutomaticEnv()
+		if err := viper.ReadInConfig(); err != nil {
+			panic(err)
+		}
+
+		viper.SetDefault("db.host", "localhost")
+		viper.SetDefault("db.port", "5432")
+		viper.SetDefault("db.name", "gotest")
+		viper.SetDefault("db.user", "gouser")
+		viper.SetDefault("db.password", "gopassword")
+		viper.SetDefault("db.sslmode", true)
+
+		instance = &Application{}
+		if err := viper.Unmarshal(instance); err != nil {
+			panic(err)
+		}
+
+		overrideIfPresentS(&instance.DB.Host, "DB_HOST")
+		overrideIfPresentS(&instance.DB.Port, "DB_PORT")
+		overrideIfPresentS(&instance.DB.Name, "DB_NAME")
+		overrideIfPresentS(&instance.DB.User, "DB_USER")
+		overrideIfPresentS(&instance.DB.Password, "DB_PASSWORD")
+		overrideIfPresentB(&instance.DB.SSLMode, "DB_SSL_MODE")
+
+	})
+	return instance
+}
+
+func (a *Application) DSN() string {
+	res := fmt.Sprintf("user=%s password=%s dbname=%s", a.DB.User, a.DB.Password, a.DB.Name)
+	if !a.DB.SSLMode {
+		res = res + " sslmode=disable"
+	}
+	return res
+}
+
+func overrideIfPresentS(target *string, key string) {
+	if viper.IsSet(key) {
+		val := viper.GetString(key)
+		*target = val
+	}
+}
+
+func overrideIfPresentB(target *bool, key string) {
+	if viper.IsSet(key) {
+		val := viper.GetBool(key)
+		*target = val
+	}
 }
